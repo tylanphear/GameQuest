c9e2bba6a41c6c1189afb786197148ad7bc51bea
Tyg13 <animemachine13@gmail.com>
2016-11-06 04:57:13 -0500

    Added New Game and Character Creation

--- Game.py
+++ Game.py
@@ -8,42 +8,64 @@ from cocos.scenes import FadeTransition
 from cocos.audio.pygame.mixer import Sound
 from cocos.audio.pygame import mixer
 
+
 class Audio(Sound):
     def __init__(self, file):
         super().__init__(file)
 
-class NewGame(cs.scene.Scene):
-    class IntroLayer(cs.layer.Layer):
-        def __init__(self, window_size):
-            super().__init__()
-            self.window_size = window_size
-            text = cs.text.Label("A long time ago...", font_name="Arial", font_size=20, anchor_x="center",
-                                 anchor_y="center")
-            text.position = window_size[0] / 2, window_size[1] - 100
-            text.opacity = 0
-            text.do(Delay(1) + FadeIn(1) + Delay(2) + FadeOut(2))
-            self.add(text)
 
-    class NewCharacterScene(cs.scene.Scene):
-        def __init__(self, window_size):
+class CharacterCreate(cs.scene.Scene):
+    class NewCharacterLayer(cs.layer.Layer):
+        def __init__(self):
             super().__init__()
-            self.window_size = window_size
-            creation = cs.text.Label("Character Creation", font_name="Arial", font_size=30, anchor_x="center", anchor_y="center")
-            creation.position = window_size[0] / 2, window_size[1] - 50
+            self.window_size = director.get_window_size()
+            creation = cs.text.Label("Character Creation", font_name="Arial", font_size=30, anchor_x="center",
+                                     anchor_y="center")
+            creation.position = self.window_size[0] / 2, self.window_size[1] - 50
+            self.add(creation)
+
+            guy = cs.sprite.Sprite("assets/guy.png")
+            guy.position = self.window_size[0] / 2, self.window_size[1] / 2
+            self.add(guy)
 
     def __init__(self):
         super().__init__()
-        intro = self.IntroLayer(director.get_window_size())
-        self.add(intro)
+        self.add(self.NewCharacterLayer())
+
+
+class NewGame(cs.scene.Scene):
+    class IntroLayer(cs.layer.Layer):
+        is_event_handler = True
+
+        def __init__(self, window_size):
+            super().__init__()
+            self.window_size = window_size
+            self.text = cs.text.Label("A long time ago...", font_name="Arial", font_size=20, anchor_x="center",
+                                      anchor_y="center")
+            self.text.position = window_size[0] / 2, window_size[1] - 100
+            self.text.opacity = 0
+            self.add(self.text)
+
+            self.do(Delay(1) + FadeIn(1) + Delay(2) + FadeOut(2), target=self.text)
+
+        def on_key_press(self, key, modifiers):
+            if key == pyglet.window.key.ENTER:
+                director.replace(CharacterCreate())
+
+        def on_exit(self):
+            super().on_exit()
+
+    def __init__(self):
+        super().__init__()
+        self.intro = self.IntroLayer(director.get_window_size())
+        self.add(self.intro)
 
 
 class MainMenu(cs.scene.Scene):
     class Menu(cs.menu.Menu):
         def __init__(self):
             super().__init__()
-            options = []
-            options.append(MenuItem("New Game", self.new_game))
-            options.append(MenuItem("Quit", self.on_quit))
+            options = [MenuItem("New Game", self.new_game), MenuItem("Quit", self.on_quit)]
 
             self.font_item['font_size'] = 20
             self.font_item['color'] = (200, 200, 200, 255)
@@ -55,7 +77,7 @@ class MainMenu(cs.scene.Scene):
 
         def new_game(self):
             new_game = NewGame()
-            cs.director.director.run(FadeTransition(new_game))
+            director.replace(FadeTransition(new_game))
 
         def on_quit(self):
             self.get_ancestor(cs.scene.Scene).end()
@@ -65,12 +87,12 @@ class MainMenu(cs.scene.Scene):
             super().__init__()
             from random import randint, random
             for i in range(0, 5):
-                pos = (randint(0, window_size[0]),
+                pos = (randint(100, window_size[0]),
                        window_size[1] / 2 + randint(0, window_size[1] - 100))
                 cloud = cs.sprite.Sprite("assets/cloud.png")
                 cloud.position = pos
                 # Move the cloud across the screen til we hit the end, then loop it back to the beginning.
-                cloud.velocity = (100 * (random()*0.5 + 0.25), 0)
+                cloud.velocity = (100 * (random() * 0.5 + 0.25), 0)
                 cloud.do(WrappedMove(*window_size))
                 self.add(cloud)
 
@@ -95,11 +117,15 @@ class MainMenu(cs.scene.Scene):
         menu = self.Menu()
         self.add(menu)
 
+    def on_exit(self):
+        super().on_exit()
+        mixer.stop()
+        for child in self.get_children():
+            self.remove(child)
+
 
 if __name__ == "__main__":
-    pyglet.resource.path.append('.')
-    pyglet.resource.reindex()
-    director.init(width=600, height=480)
+    director.init(caption="Game Quest", width=600, height=480)
     mixer.init()
     main_menu = MainMenu()
     director.run(main_menu)
