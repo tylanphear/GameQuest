d732bb46f88733269f65b46bc334cb0e39db3980
Tyg13 <animemachine13@gmail.com>
2016-11-07 02:03:29 -0500

    Fixed cloud generation
    Cloud procedure overhauled and commented
    Clouds should try not to overlap

--- Game.py
+++ Game.py
@@ -1,16 +1,14 @@
 import string
-from copy import copy
+from random import randint, random
 
 import cocos as cs
-import math
 import pyglet
-from cocos.actions import FadeOut, WrappedMove, Delay, FadeIn, MoveTo, Driver, Move
-from cocos.menu import MenuItem
-from cocos.director import director
-from cocos.scenes import FadeTransition
-
-from cocos.audio.pygame.mixer import Sound
+from cocos.actions import FadeOut, Delay, FadeIn, Move
 from cocos.audio.pygame import mixer
+from cocos.audio.pygame.mixer import Sound
+from cocos.director import director
+from cocos.menu import MenuItem
+from cocos.scenes import FadeTransition
 
 
 class Audio(Sound):
@@ -19,50 +17,79 @@ class Audio(Sound):
 
 
 class CharacterCreate(cs.scene.Scene):
+    """
+    This is the character creation scene where the player inputs their name, chooses their class and other variables
+    which are to be determined.
+    """
     class KBLayer(cs.layer.Layer):
+        """
+        Just a basic keyboard handling layer. We use this to update the text containing the player's attributes using
+        the modifiers and ascii keys.
+        """
         is_event_handler = True
 
         def __init__(self):
+            """
+            Creates the keyboard layer with a set of pressed keys and modifiers (default empty)
+            """
             super().__init__()
-            self.keys_pressed = set()
+            self.keys_pressed = {}
             self.modifiers = set()
             self.count = 0
-            self.pressing = False
+            self.pressing = True
+            self.holding = False
+            self.held_time = 0
 
         def on_key_press(self, key, modifiers):
+            """
+            When a key is pressed, just add it to the set of keys. Duplicate keys will not be added twice (since it's a
+            set)
+            """
             if key == pyglet.window.key.ENTER and self.parent.black is not None:
                 self.parent.remove(self.parent.black)
                 self.parent.black = None
-            self.pressing = 10
-            self.keys_pressed.add(key)
+            self.keys_pressed[pyglet.window.key.symbol_string(key)] = True
             self.modifiers = modifiers
-            self.update_text()
+            self.pressing = True
+            self.update_text(0)
+            self.schedule(self.check_holding)
 
-        def on_draw(self):
-            self.update_text()
+        def check_holding(self, dt):
+            if self.pressing and not self.holding:
+                self.held_time += dt
+                if self.held_time > 0.5:
+                    self.holding = True
+                    self.unschedule(self.check_holding)
+                    self.schedule_interval(self.update_text, 0.1)
 
-        def update_text(self):
-            if self.pressing is not True:
+        def update_text(self, dt):
+            """
+            Takes each key and then checks the modifiers to determine whether it will be capital or lowercase, then
+            adds it to the current attribute. If the key is
+            :return:
+            """
+            if not self.pressing or not self.holding:
                 return
-            keys = [k for k in self.keys_pressed if pyglet.window.key.symbol_string(k) in string.ascii_letters]
-            for k in keys:
-                if self.modifiers is not None and self.modifiers & pyglet.window.key.LSHIFT:
-                    self.parent.name_in.element.text += pyglet.window.key.symbol_string(k)
-                else:
-                    self.parent.name_in.element.text += pyglet.window.key.symbol_string(k).lower()
-            self.count += len(keys)
-            if pyglet.window.key.SPACE in self.keys_pressed:
-                self.parent.name_in.element.text += " "
-                self.count += 1
-            if pyglet.window.key.BACKSPACE in self.keys_pressed and self.count != 0:
-                self.count -= 1
-                self.parent.name_in.element.text = self.parent.name_in.element.text[:-1]
+            for k in filter(lambda x: self.keys_pressed[x] is True, self.keys_pressed):
+                if k in string.ascii_letters:
+                    if self.modifiers is not None and self.modifiers & pyglet.window.key.LSHIFT:
+                        self.parent.name_in.element.text += k
+                    else:
+                        self.parent.name_in.element.text += k.lower()
+                    self.count += 1
+                if k == "SPACE":
+                    self.parent.name_in.element.text += " "
+                    self.count += 1
+                if k == "BACKSPACE" and self.count != 0:
+                    self.count -= 1
+                    self.parent.name_in.element.text = self.parent.name_in.element.text[:-1]
+            self.held_time = 0
 
         def on_key_release(self, key, modifiers):
-            self.pressing = False
-            if key in self.keys_pressed:
-                self.keys_pressed.remove(key)
+            self.keys_pressed[pyglet.window.key.symbol_string(key)] = False
             self.modifiers = None
+            self.holding = False
+            self.held_time = 0
 
     def __init__(self):
         super().__init__()
@@ -85,18 +112,7 @@ class CharacterCreate(cs.scene.Scene):
 
         self.black = cs.layer.ColorLayer(255, 255, 255, 255)
         self.add(self.black)
-
-        self.schedule_interval(self.fade_in, 0.1)
-
-    def fade_in(self, dt):
-        if self.black is None:
-            self.unschedule(self)
-            return
-
-        if self.black.opacity > 1:
-            self.black.opacity -= 10*dt
-        else:
-            self.remove(self.black)
+        self.black.do(Delay(0.5) + FadeOut(10))
 
 
 class NewGame(cs.scene.Scene):
@@ -153,7 +169,13 @@ class MainMenu(cs.scene.Scene):
             self.get_ancestor(cs.scene.Scene).end()
 
     class CloudLayer(cs.layer.Layer):
+        """"
+        The clouds behind the menu text.
+        """
         def __init__(self, num_clouds):
+            """"
+            Places initial clouds.
+            """
             from time import clock
             from random import seed
             seed(clock())
@@ -161,46 +183,73 @@ class MainMenu(cs.scene.Scene):
             self.window_size = director.get_window_size()
             self.clouds = []
             self.num_clouds = num_clouds
-            while len(self.clouds) < self.num_clouds:
-                self.make_cloud(True)
+            while len(self.children) < self.num_clouds:
+                self.make_cloud(inital_gen=True)
 
             self.schedule(self.update)
 
-        def make_cloud(self, is_random_placement=False):
-            from random import randint, random
-            pos = 0
-            cloud = cs.sprite.Sprite("assets/cloud.png")
-            placed = False
-            while not placed:
-                if is_random_placement:
-                    pos = (randint(cloud.width, self.window_size[0]) - cloud.width,
-                           randint(self.window_size[1] / 2, self.window_size[1]) - cloud.height / 2)
+        def make_cloud(self, inital_gen=False):
+            """"
+            Places a cloud in a random location, hopefully not intersecting any other clouds, and sends it rightwards.
+            """
+
+            def random_location(scatter):
+                """"
+                Helper function.
+                Generates a random location, putting it random on the screen if scattering, on the left edge if not.
+                """
+                # The first time generating the clouds, we want them all over the screen
+                if scatter:
+                    pos = (randint(-cloud.width, self.window_size[0] + cloud.width / 2),
+                           randint(cloud.height / 2 + self.window_size[1] / 2 - 100,
+                                   self.window_size[1] - cloud.height / 2))
+                # The rest of the clouds will be generated starting from the left edge
                 else:
                     pos = (-cloud.width,
-                           randint(self.window_size[1] / 2, self.window_size[1]) - cloud.height / 2)
-                if len(self.clouds) == 0:
-                    placed = True
-                else:
+                           randint(cloud.height / 2 + self.window_size[1] / 2 - 100,
+                                   self.window_size[1] - cloud.height / 2))
+                return pos
+
+            cloud = cs.sprite.Sprite("assets/cloud.png")
+            # If there's no other clouds, just place it
+            if len(self.clouds) == 0:
+                cloud.position = random_location(inital_gen)
+            # This algorithm tries to place the cloud in its own location
+            # without overlapping any clouds, but will give up if this is impossible.
+            else:
+                placed = False
+                fails = 0
+                # Try to place it so it doesn't intersect any other clouds
+                while not placed:
+                    cloud.position = random_location(inital_gen)
                     for c in self.clouds:
-                        if c.get_rect().contains(*pos):
-                            placed = False
-                        else:
+                        # If we've failed to place the cloud too many times, just place it
+                        if fails > len(self.clouds):
                             placed = True
-            cloud.position = pos
-            # Move the cloud across the screen til we hit the end, then loop it back to the beginning.
-            cloud.velocity = (25*(random()*0.5 + 0.5), 0)
+                            break
+                        # If the cloud is in another cloud, we've failed again and we try another spot
+                        elif c.get_rect().intersects(cloud.get_rect()):
+                            placed = False
+                            fails += 1
+                            break
+                        # Otherwise, so far so good, keep checking other clouds
+                        placed = True
+            # 25 times (1.5 to 0.5) in the x direction
+            # -2 to 3 in the y direction
+            cloud.velocity = (25 * (random() * 0.5 + 0.5), -2 + 5 * random())
+            # Move it to the right side of the window to the same y position as it started in
             cloud.do(Move(position=(self.window_size[0], cloud.position[1])))
-            self.clouds.append(cloud)
             self.add(cloud)
 
         def update(self, dt):
-            while len(self.clouds) < self.num_clouds:
+            """"
+            Move the clouds, and generate new ones if any exitted the screen
+            """
+            while len(self.children) < self.num_clouds:
                 self.make_cloud()
-            clouds_copy = copy(self.clouds)
-            for cloud in clouds_copy:
-                if cloud.position[0]-cloud.width/2 > self.window_size[0]:
+            for index, cloud in self.children:
+                if cloud.position[0] - cloud.width / 2 > self.window_size[0]:
                     self.remove(cloud)
-                    self.clouds.remove(cloud)
 
     def __init__(self):
         super().__init__()
@@ -212,7 +261,7 @@ class MainMenu(cs.scene.Scene):
         song = Audio("assets/menu.ogg")
         song.play(-1)
 
-        clouds = self.CloudLayer(10)
+        clouds = self.CloudLayer(30)
         self.add(clouds)
 
         label = cs.text.Label("Game Quest", font_name="Arial", font_size=30, anchor_x="center", anchor_y="center")
@@ -232,6 +281,7 @@ class MainMenu(cs.scene.Scene):
 
 if __name__ == "__main__":
     director.init(caption="Game Quest", width=600, height=480)
+    director.show_FPS = True
     mixer.init()
     main_menu = MainMenu()
     director.run(main_menu)
